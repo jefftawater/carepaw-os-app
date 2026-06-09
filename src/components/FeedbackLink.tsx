5e9e6b584fb90cb8fc4c8cca496017e5b5c930b9
Jeff Tawater <jeff.tawater@gmail.com>
2026-06-09 07:16:08 -0500

    Fix CarePaw OS feedback email

--- src/components/FeedbackLink.tsx
+++ src/components/FeedbackLink.tsx
@@ -1,5 +1,5 @@
 const feedbackHref =
-  "mailto:carepawos@gmail.com?subject=CarePaw%20OS%20beta%20feedback&body=What%20felt%20helpful%3F%20What%20felt%20confusing%3F%20What%20did%20you%20need%20that%20wasn%27t%20here%3F";
+  "mailto:hello@carepawos.com?subject=CarePaw%20OS%20beta%20feedback&body=What%20felt%20helpful%3F%20What%20felt%20confusing%3F%20What%20did%20you%20need%20that%20wasn%27t%20here%3F";
 
 export function FeedbackLink() {
   return (
